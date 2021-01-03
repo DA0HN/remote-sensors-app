506098e4dbf5f625519d8f5047449dcbf06fde33
Gabriel Honda <gabrielhondacba@gmail.com>
2021-01-03 17:44:06 -0400

    feat: adiciona configuração para DateTime
    Formata horário por padrão em `pt_BR`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:remote_sensors_app/module/sensor/sensor_binding.dart';
 
@@ -12,6 +13,11 @@ class RemoteSensorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
       title: 'Remote Sensor App',
       debugShowCheckedModeBanner: false,
       initialBinding: SensorBinding(),
