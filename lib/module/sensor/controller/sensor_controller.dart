1b41908c77e1e72163cbd64b6a87f926c8942c65
Gabriel Honda <gabrielhondacba@gmail.com>
2021-01-03 18:39:13 -0400

    refactor: remove código desnecessário

--- lib/module/sensor/controller/sensor_controller.dart
+++ lib/module/sensor/controller/sensor_controller.dart
@@ -9,22 +9,13 @@ import '../../../data/repository/sensor_repository.dart';
 class SensorController extends GetxController {
   final SensorRepository repository;
 
-  // final RxList<SensorModel> _data = List<SensorModel>().obs;
   final _data = <SensorModel>[];
   SensorModel _currentModel = SensorModel(temperature: null, date: null);
   bool _isInLoopUpdate = false;
   Timer _loopUpdateTask;
   final int _seconds = 10;
 
-  SensorController({@required this.repository}) {
-    // Timer.periodic(Duration(seconds: 5), (timer) {
-    //   updateChart();
-    // });
-  }
-
-  // List<SensorModel> get data => _data.value;
-
-  // set data(value) => this._data.assignAll(value);
+  SensorController({@required this.repository});
 
   Future<void> updateChart() async {
     final model = repository.currentTemperature();
